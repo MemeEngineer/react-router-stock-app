a98a0c16ce2d917686e837a23cc2a3df65757494
JWu <johnnyw131@gmail.com>
2023-08-09 01:15:50 -0400

    Able to Fetch Data and hidden API key

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,7 +1,41 @@
+import {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+
 export default function DashBoard(){
+const [stocks, setStock] = useState([])
+const apiKey = process.env.REACT_APP_STOCKAPI_KEY
+const params = useParams()
+const{symbol} = params;
+
+const url = `https://financialmodelingprep.com/api/v3/available-traded/list?&limit=10&apikey=${apiKey}`
+
+const getStock = async() => {
+    try{
+    const res = await fetch(url);
+    const data = await res.json();
+    setStock(data);
+    console.log(data)
+    }catch(e){
+        console.log('Error Fetching Data', e)
+    }
+   
+}
+
+useEffect(()=> {
+    getStock()
+},[])
+
+
     return(
         <div>
             <h1>Dash</h1>
+            {stocks.map((stock)=> {
+                return(
+                    <div>
+                        {stock.symbol}
+                        </div>
+                )
+            })}
         </div>
     )
 }
